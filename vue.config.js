5eca0db02a2dd15ccd867943f3ac307b7b4dba00
liuyi <liuxy0551@qq.com>
2019-12-12 14:21:59 +0800

    update: github page

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,7 @@ module.exports = {
   productionSourceMap: false, // 生产环境的 source map
   devServer: {
     disableHostCheck: true,//解决127.0.0.1指向其他域名时出现"Invalid Host header"问题
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production' ? '/h5-camera/' : '/',
+  outputDir: 'docs'
 }
